28547446a7a4ff15ca483090eb91c8be9304bf54
IKBOstudent <90282668+IKBOstudent@users.noreply.github.com>
2023-04-23 21:27:44 +0300

    Initial commit

---
+++ src/main/java/com/example/shppyad19/Shppyad19Application.java
@@ -0,0 +1,13 @@
+package com.example.shppyad19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shppyad19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shppyad19Application.class, args);
+    }
+
+}
